5dd5661f0f55c79130c83fc07852ebf2eb08dee4
eliji9 <eliji@gmail.com>
2019-07-02 20:58:36 +0000

    commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("com.perceptio.training.zpedidos_vtas.controller.App", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/perceptio/training/zpedidos_vtas/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
